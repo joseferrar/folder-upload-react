edf7673d9ba0b163e7a0100d785e8444622a4e20
Godwinjose <godwinjoshwa.s@colanonline.com>
2022-10-20 10:45:34 +0530

    folder upload input working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [changeFolder, setChangeFolder] = useState([]);
+
+  const folderChange = async (e) => {
+    console.log(e.target.files);
+
+    Array.from(e.target.files).forEach((file) => {
+      let reader = new FileReader();
+      setChangeFolder(e.target.files);
+      reader.onload = () => {
+        console.log(reader.result);
+      };
+      reader.readAsText(file);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: 30 }}>
+      <input
+        type="file"
+        id="flup"
+        webkitdirectory="true"
+        onChange={(e) => folderChange(e)}
+      />
+
+      <ul>
+        {Array.from(changeFolder).map((file, i) => (
+          <li key={i}>{file.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
