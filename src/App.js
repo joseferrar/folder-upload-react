c7c72cce21dcbacd54b59b845bbecaf5b41dce0e
Godwinjose <godwinjoshwa.s@colanonline.com>
2022-10-20 20:06:08 +0530

    folder txts find

--- src/App.js
+++ src/App.js
@@ -2,20 +2,42 @@ import React, { useState } from "react";
 
 function App() {
   const [changeFolder, setChangeFolder] = useState([]);
+  const [todoArr, setTodo] = useState([]);
+  const [text, setText] = useState("");
+  const [search, setSearch] = useState("");
+  const [replace, setReplace] = useState("");
 
-  const folderChange = async (e) => {
+  const folderChange = (e) => {
     console.log(e.target.files);
 
     Array.from(e.target.files).forEach((file) => {
       let reader = new FileReader();
       setChangeFolder(e.target.files);
-      reader.onload = () => {
-        console.log(reader.result);
+
+      reader.onloadend = () => {
+        const text1 = reader.result;
+        todoArr.push({ text: text1, file: file.name });
+        console.log(todoArr);
+        setText(text1);
       };
       reader.readAsText(file);
     });
   };
+  const searchFilter = todoArr.filter((value) => {
+    if (search === !null) {
+      return value.text;
+    } else if (value.text.toLowerCase().includes(search.toLowerCase()))
+      return value.text;
+  });
+  console.log(searchFilter);
 
+  const replaceFunction = () => {
+    searchFilter.map((item) => {
+      const newText = item.text.replace(search, replace);
+      console.log(newText);
+      setReplace(newText);
+    });
+  };
   return (
     <div style={{ margin: 30 }}>
       <input
@@ -25,11 +47,27 @@ function App() {
         onChange={(e) => folderChange(e)}
       />
 
+      <input
+        type="text"
+        value={search}
+        placeholder="search content"
+        onChange={(e) => setSearch(e.target.value)}
+      />
       <ul>
-        {Array.from(changeFolder).map((file, i) => (
-          <li key={i}>{file.name}</li>
+        {searchFilter.map((item, i) => (
+          <div>
+            <h2>{item.file}</h2>
+            <li key={i}>{item.text}</li>
+          </div>
         ))}
       </ul>
+      <input
+        type="text"
+        placeholder="replace text"
+        value={replace}
+        onChange={(e) => setReplace(e.target.value)}
+      />
+      <button onClick={replaceFunction}>Replace</button>
     </div>
   );
 }
